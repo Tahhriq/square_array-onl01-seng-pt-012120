576f965a7727a66a43063e4b5d440c823d02a5fa
Tariq M Sulaiman <tariqsulaiman11@gmail.com>
2020-03-15 06:19:42 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -11,7 +11,6 @@ end
 def square_array(array)
   squared_array = []
   array.each do |number|
-    squared_array.push(Integer(number)**2)
-  end
+    squared_array << number
   return squared_array
 end
